6133bb3cd98c0b14ca321e0d6cb51c1e9db41a3c
Keisuke Kiriyama <krkr.xm.rkr7x@gmail.com>
2017-04-19 19:48:48 +0900

    25-26

--- 25.py
+++ 25.py
@@ -3,21 +3,33 @@ import re
 from collections import OrderedDict
 
 
+# def get_template():
+#     dict = OrderedDict()
+#     text = extract_from_json('イギリス')
+#     base_info = re.search(u'(?<=\{\{基礎情報).+?(?=\}\}\n)', text, re.DOTALL)
+#     if base_info:
+#         pattern = re.compile(r'\|(.+?) = (.+?)(?<!<br/>)\n')
+#         temp_list = pattern.findall(base_info.group(0))
+#         if temp_list:
+#             for temp in temp_list:
+#                 dict[temp[0]] = temp[1]
+#             print(dict)
+#         else:
+#             print('error')
+#     else:
+#         print('error')
+
+
 def get_template():
     dict = OrderedDict()
     text = extract_from_json('イギリス')
-    base_info = re.search(u'(?<=\{\{基礎情報).+?(?=\}\}\n)', text, re.DOTALL)
-    if base_info:
-        pattern = re.compile(r'\|(.+?) = (.+?)(?<!<br/>)\n')
-        temp_list = pattern.findall(base_info.group(0))
-        if temp_list:
-            for temp in temp_list:
-                dict[temp[0]] = temp[1]
-            print(dict)
-        else:
-            print('error')
-    else:
-        print('error')
+    base_info = re.search(u'(?<=\{\{基礎情報)(.+?)(?=\}\}\n)', text, re.DOTALL)
+    pattern = re.compile(u'\|(.+?) = (.+?)(?<!<br/>)\n')
+    info_list = pattern.findall(base_info.group(1))
+    if info_list:
+        for info in info_list:
+            dict[info[0]] = info[1]
+    print(dict)
 
 
 
--- 26.py
+++ 26.py
@@ -3,23 +3,40 @@ import re
 from collections import OrderedDict
 
 
+# def get_template():
+#     dict = OrderedDict()
+#     text = extract_from_json('イギリス')
+#     base_info = re.search(u'(?<=\{\{基礎情報).+?(?=\}\}\n)', text, re.DOTALL)
+#     if base_info:
+#         pattern = re.compile(r'\|(.+?) = (.+?)(?<!<br/>)\n')
+#         temp_list = pattern.findall(base_info.group(0))
+#         if temp_list:
+#             for temp in temp_list:
+#                 tt = temp[1]
+#                 tt = re.sub(r"'{2,5}", r"", tt)
+#                 dict[temp[0]] = tt
+#             print(dict)
+#         else:
+#             print('error')
+#     else:
+#         print('error')
+
+
 def get_template():
     dict = OrderedDict()
     text = extract_from_json('イギリス')
-    base_info = re.search(u'(?<=\{\{基礎情報).+?(?=\}\}\n)', text, re.DOTALL)
-    if base_info:
-        pattern = re.compile(r'\|(.+?) = (.+?)(?<!<br/>)\n')
-        temp_list = pattern.findall(base_info.group(0))
-        if temp_list:
-            for temp in temp_list:
-                tt = temp[1]
-                tt = re.sub(r"'{2,5}", r"", tt)
-                dict[temp[0]] = tt
-            print(dict)
-        else:
-            print('error')
-    else:
-        print('error')
+    base_info = re.search(u'(?<=\{\{基礎情報)(.+?)(?=\}\}\n)', text, re.DOTALL)
+    pattern = re.compile(u'\|(.+?) = (.+?)(?<!<br/>)\n')
+    info_list = pattern.findall(base_info.group(1))
+    emphasis_pattern = re.compile(r"'{2,5}(.+)'{2,5}")
+    if info_list:
+        for info in info_list:
+            emphasis = emphasis_pattern.sub(r'', info[1])
+        
+            # emphasis_pattern.sub(r"\2", info[1])
+            # print(info[1])
+            # dict[info[0]] = info[1]
+
 
 
 
